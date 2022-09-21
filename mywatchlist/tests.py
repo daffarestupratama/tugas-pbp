cbc5863f29b34c7a3dc869113c34c6d4e4a86db1
daffarestupratama <daffa.ilham@ui.ac.id>
2022-09-22 06:43:21 +0700

    adding testing unit

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+class MywatchlistTest(TestCase):
+
+    def test_watchlist_show_html(self):         
+        response = Client().get('/mywatchlist/html/')
+        self.assertEquals(response.status_code,200)
+    
+    def test_watchlist_show_xml(self):         
+        response = Client().get('/mywatchlist/json/')
+        self.assertEquals(response.status_code,200)
+    
+    def test_watchlist_show_json(self):         
+        response = Client().get('/mywatchlist/xml/')
+        self.assertEquals(response.status_code,200)
