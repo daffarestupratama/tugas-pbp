4d1d8ad6fd9052aee025e4cbbe79aa26a53cfd87
daffarestupratama <daffa.ilham@ui.ac.id>
2022-09-29 08:41:03 +0700

    Fix typo in TaskForm

--- todolist/forms.py
+++ todolist/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class TaskForm(forms.Form):
-    title = forms.CharField(label='judul', max_length=200)
+    title = forms.CharField(max_length=200)
     description = forms.CharField()
